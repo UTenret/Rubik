3de5f99369e2a654125f4a581d32713b80f9be0c
UlysseTenret <ulysse.tenret@gmail.com>
2024-05-30 16:37:04 +0200

    merged all lut generating bfs into one

--- srcs/PruningTable.hpp
+++ srcs/PruningTable.hpp
@@ -5,12 +5,19 @@
 #include <queue>
 #include <set>
 #include <cstring>
+#include <functional>
 #include "RubiksCube.hpp"
 
 
 class PruningTable {
 public:
     explicit PruningTable(const RubiksCube& cube);
+
+	using CalculateIndexFunc = std::function<int(const RubiksCube&)>;
+
+	void bfsGenerateLUT(std::vector<int>& lut,
+						CalculateIndexFunc calculateIndex,
+						const std::vector<std::string>& moves);
 
     void generateLUT();
     std::vector<int> loadLUT(const std::string& filename, int size);
@@ -21,8 +28,4 @@ private:
 	RubiksCube cube;
 	std::vector<std::vector<int>> luts;
 
-    void bfsGenerateLUTG0();
-    void bfsGenerateLUTG1();
-	void bfsGenerateLUTG2();
-	void bfsGenerateLUTG3();
 };
